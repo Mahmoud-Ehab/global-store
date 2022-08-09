974d94e173cd311d74a41289c99c379a10043ae5
Mahmoud Ehab <mo.ehab150@gmail.com>
2022-08-09 06:47:44 +0200

    directort renamed

--- backend/src/Database/postgres/init.sh
+++
@@ -1,8 +0,0 @@
-databasename="globalstore"
-
-createdb ${databasename}
-
-psql -d ${databasename} -f initRelations.sql
-psql -d ${databasename} -f initConstraints.sql
-psql -d ${databasename} -f initFunctions.sql
-psql -d ${databasename} -f initTriggers.sql
--- backend/src/Database/postgres/initTriggers.sql
+++
@@ -1,4 +0,0 @@
-CREATE TRIGGER banUser 
-  AFTER UPDATE OF TRW ON users
-  FOR ROW WHEN ( NEW.TRW >= 10 )
-  EXECUTE FUNCTION ban_user();
