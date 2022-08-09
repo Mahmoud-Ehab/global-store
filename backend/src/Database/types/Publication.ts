0d3eacb3066d54a546a028a19b8788ee4e29b782
Mahmoud Ehab <mo.ehab150@gmail.com>
2022-08-09 06:52:12 +0200

    types have been moved to database directory

---
+++ backend/src/Database/types/Publication.ts
@@ -0,0 +1,11 @@
+type Publication = {
+  id: number,
+  user_id: number,
+  title: string,
+  description: string,
+  cost: number,
+  currency: string,
+  phone: string,
+}
+
+export default Publication;
---
+++ backend/src/Database/types/Review.ts
@@ -0,0 +1,9 @@
+type Review = {
+  id: number,
+  user_id: number,
+  publication_id: number,
+  title: string,
+  body: string,
+}
+
+export default Review;
