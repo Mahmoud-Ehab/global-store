b58fecb1059ded6fb2ae0e13eb598a9f25d14011
Mahmoud Ehab <mo.ehab150@gmail.com>
2022-08-18 14:38:44 +0200

    UserRouter init implementation

--- backend/src/server/RouterFactory/routers/UserRouter.ts
+++ backend/src/server/RouterFactory/routers/UserRouter.ts
@@ -3,7 +3,11 @@ import RouterFactory from "../RouterFactory";
 class UserRouter extends RouterFactory {
   init() {
     this.get('/get/:userid', (_req, _res) => {
-      _res.send("test");
+      const { userid } = JSON.parse(_req.params.toString());
+      this.queryManager.query(async () => { 
+        const userdata = await this.queryManager.users.get(userid);
+        _res.json(userdata);
+      });
     });
   }
 }
