0ed9adf9ae6202fd4b184ce93225404af9118adf
Mahmoud Ehab <mo.ehab150@gmail.com>
2022-08-27 14:46:50 +0200

    app.ts using PublicationRouterFactor

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,13 +2,15 @@ import QueryManager from './database/QueryManager/QueryManager'
 import Server from './server/Server';
 
 import UserRouterFactory from './server/RouterFactory/factories/UserRouterFactory';
+import PublicationRouterFactory from './server/RouterFactory/factories/PublicationRouterFactory';
 
 const server = new Server("localhost", 5000);
 const queryManager = new QueryManager();
 const userRF = new UserRouterFactory(queryManager);
+const pubRF = new PublicationRouterFactory(queryManager);
 
 server.addRouter(userRF);
-
+server.addRouter(pubRF);
 server.start();
 
 process.on('SIGTERM', () => {
