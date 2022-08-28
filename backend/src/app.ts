4214b9a036c4389dd2447d9372ecf3432ce16ed9
Mahmoud Ehab <mo.ehab150@gmail.com>
2022-08-28 17:57:22 +0200

    ReviewRouterFactory has been added to app.js

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,14 +3,18 @@ import Server from './server/Server';
 
 import UserRouterFactory from './server/RouterFactory/factories/UserRouterFactory';
 import PublicationRouterFactory from './server/RouterFactory/factories/PublicationRouterFactory';
+import ReviewRouterFactory from './server/RouterFactory/factories/ReviewRouterFactory';
 
 const server = new Server("localhost", 5000);
 const queryManager = new QueryManager();
+
 const userRF = new UserRouterFactory(queryManager);
 const pubRF = new PublicationRouterFactory(queryManager);
+const reviewRF = new ReviewRouterFactory(queryManager);
 
 server.addRouter(userRF);
 server.addRouter(pubRF);
+server.addRouter(reviewRF);
 server.start();
 
 process.on('SIGTERM', () => {
