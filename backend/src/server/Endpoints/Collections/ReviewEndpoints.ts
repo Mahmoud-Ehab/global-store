1da1e085cd2520da13284abd5293ae5516133e2c
Mahmoud Ehab <mo.ehab150@gmail.com>
2022-08-28 17:56:03 +0200

    Endpoints implementation is complete

---
+++ backend/src/server/Endpoints/Collections/ReviewEndpoints.ts
@@ -0,0 +1,37 @@
+import { Endpoint } from "../type";
+
+export const getReview: Endpoint = {
+  path: (pubid, userid) => `/review/of/publication/${pubid}/of/user/${userid}`,
+  expressPath: "/of/publication/:pubid/of/user/:userid",
+  type: "GET"
+}
+
+export const getReviewsOfPublication: Endpoint = {
+  path: (pubid) => `/review/of/publication/${pubid}`,
+  expressPath: "/of/publication/:pubid",
+  type: "GET"
+}
+
+export const getReviewsOfUser: Endpoint = {
+  path: (userid) => `/review/of/user/${userid}`,
+  expressPath: "/of/user/:userid",
+  type: "GET"
+}
+
+export const create: Endpoint = {
+  path: () => `/review/create`,
+  expressPath: "/create",
+  type: "POST"
+}
+
+export const update: Endpoint = {
+  path: () => `/review/update`,
+  expressPath: "/update",
+  type: "PATCH"
+}
+
+export const remove: Endpoint = {
+  path: () => `/review/delete`,
+  expressPath: "/delete",
+  type: "DELETE"
+}
--- backend/src/server/Endpoints/index.ts
+++ backend/src/server/Endpoints/index.ts
@@ -1,5 +1,7 @@
 import * as UserEP from './Collections/UserEndpoints'
 import * as PubEP from './Collections/PublicationEndpoints'
+import * as ReviewEP from './Collections/ReviewEndpoints'
 
 export const UserEndpoints = UserEP;
 export const PublicationEndpoints = PubEP;
+export const ReviewEndpoints = ReviewEP;
