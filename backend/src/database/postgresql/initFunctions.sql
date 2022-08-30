a125ded56cd6c8b8c59f68599984ce056419006a
Mahmoud Ehab <mo.ehab150@gmail.com>
2022-08-30 14:48:23 +0200

    new triggers have been added on users and publications delete queries

--- backend/src/database/postgresql/initFunctions.sql
+++ backend/src/database/postgresql/initFunctions.sql
@@ -1,4 +1,5 @@
-CREATE OR REPLACE FUNCTION ban_user() RETURNS TRIGGER AS $ban_user$
+CREATE OR REPLACE FUNCTION ban_user() 
+RETURNS TRIGGER AS $ban_user$
 BEGIN
   UPDATE users
   SET users.banned = TRUE
@@ -7,3 +8,22 @@ BEGIN
   RETURN NEW;
 END;
 $ban_user$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION delete_user() 
+RETURNS TRIGGER AS $delete_user$
+BEGIN
+  DELETE FROM publications WHERE user_id = OLD.id;
+  RETURN OLD;
+END;
+$delete_user$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION delete_publication() 
+RETURNS TRIGGER AS $delete_publication$
+BEGIN
+  DELETE FROM reviews WHERE publication_id = OLD.id;
+  RETURN OLD;
+END;
+$delete_publication$ LANGUAGE plpgsql;
+
--- backend/src/database/postgresql/initTriggers.sql
+++ backend/src/database/postgresql/initTriggers.sql
@@ -2,3 +2,13 @@ CREATE TRIGGER banUser
   AFTER UPDATE OF TRW ON users
   FOR ROW WHEN ( NEW.TRW >= 10 )
   EXECUTE FUNCTION ban_user();
+
+CREATE TRIGGER onDeleteUser 
+  BEFORE DELETE ON users
+  FOR EACH ROW
+  EXECUTE FUNCTION delete_user();
+
+CREATE TRIGGER onDeletePublication
+  BEFORE DELETE ON publications
+  FOR EACH ROW
+  EXECUTE FUNCTION delete_publication();
