aac25078c5f518c5d314340b70eaa2708c61fad0
Mahmoud Ehab <mo.ehab150@gmail.com>
2022-09-11 02:09:48 +0200

    the publication router new endpoints have been implemented. And some fixes in credentails object and BadRequest errors

--- backend/src/server/RouterFactory/factories/PublicationRouterFactory.ts
+++ backend/src/server/RouterFactory/factories/PublicationRouterFactory.ts
@@ -32,14 +32,47 @@ class PublicationRouterFactory extends RouterFactoryImp {
     });
     
 
-
-    /*** get publications of a certain user ***/
-    this.get(PublicationEndpoints.getPublicationsOfUser, (req, res, next) => {
-      const userid = parseInt(req.params.userid);
-      if (isNaN(userid)) {
+    /*** get list of publications with a specified limit ***/
+    this.get(PublicationEndpoints.getPublicationsWithLimit, (req, res, next) => {
+      const limit = parseInt(req.params.limit);
+      if (isNaN(limit)) {
         next(BadRequest);
         return;
       }
+      this.queryManager.query(async () => {
+        const publications = await this.queryManager.publications.getLimit(limit);
+        res.json(Done({data: publications}));
+      })
+      .execute()
+      .catch(e => next(DBError(e.code)))
+    });
+
+
+    /*** get list of publications with a specified limit and offset ***/
+    this.get(PublicationEndpoints.getPublicationsWithLimitAndOffset, (req, res, next) => {
+      const limit = parseInt(req.params.limit);
+      const offset = parseInt(req.params.offset);
+      if (isNaN(limit) || isNaN(offset)) {
+        next(BadRequest);
+        return;
+      }
+      this.queryManager.query(async () => {
+        const publications = await this.queryManager.publications.getLimitWithOffset(limit, offset);
+        res.json(Done({data: publications}));
+      })
+      .execute()
+      .catch(e => next(DBError(e.code)))
+    });
+
+
+    /*** get publications of a certain user ***/
+    this.get(PublicationEndpoints.getPublicationsOfUser, (req, res, next) => {
+      const userid = req.params.userid;
+      if (!userid) {
+        next(BadRequest);
+        return;
+      }
+
       this.queryManager.query(async () => {
         const pubs = await this.queryManager.publications.getFiltered({
           user_id: userid
@@ -54,21 +87,22 @@ class PublicationRouterFactory extends RouterFactoryImp {
 
     /*** create a new publication in the database ***/
     this.post(PublicationEndpoints.create, (req, res, next) => {
+      const body = req.body;
+      const cred = req.body.credentials;
+
       const required = {
-        //@TODO: remove id attribute
-        id: req.body.id,
-        user_id: req.body.user_id,
-        title: req.body.title,
-        price: req.body.price,
-        currency: req.body.currency,
+        user_id: body.user_id,
+        title: body.title,
+        price: body.price,
+        currency: body.currency,
       }
       const sub = {
-        description: req.body.description,
-        phone: req.body.phone
+        description: body.description,
+        phone: body.phone
       }
       const credentials = {
-        username: req.body.username,
-        password: req.body.password,
+        username: cred ? cred.username : undefined,
+        password: cred ? cred.password : undefined,
       }
       const pubBody = {...required, ...sub}
 
@@ -81,7 +115,10 @@ class PublicationRouterFactory extends RouterFactoryImp {
 
       this.queryManager.query(async () => {
         const user = await this.queryManager.users.get(pubBody.user_id);
-
+        if (!user.id) {
+          next(NotFound);
+          return;
+        }
         if (!this.auth(user, credentials)) {
           next(AuthenticationFailed);
           return;
@@ -95,6 +132,51 @@ class PublicationRouterFactory extends RouterFactoryImp {
     })
 
 
+    this.patch(PublicationEndpoints.update, (req, res, next) => {
+      const required = {
+        id: req.body.id,
+        user_id: req.body.user_id,
+        data: req.body.data
+      }
+      const cred = req.body.credentials;
+      const credentials = {
+        username: cred ? cred.username : undefined,
+        password: cred ? cred.password : undefined
+      }
+      if (this.hasUndefined(required, credentials)) {
+        next(BadRequest);
+        return;
+      }
+
+      // Ensure that there are no id entries in required.data
+      if (required.data.id !== undefined || required.data.user_id !== undefined) {
+        next(BadRequest);
+        return;
+      }
+
+      //@TODO: encrypt password
+
+      this.queryManager.query(async () => {
+        const user = await this.queryManager.users.get(required.user_id);
+        if (!user.id) {
+          next(NotFound);
+          return;
+        }
+        if (!this.auth(user, credentials)) {
+          next(AuthenticationFailed);
+          return;
+        }
+        const publication = await this.queryManager.publications.get(required.id);
+        if (!publication.id) {
+          next(NotFound);
+          return;
+        }
+        await this.queryManager.publications.update(required.data, {id: required.id});
+        next(Done());
+      }).execute()
+      .catch(e => next(DBError(e.code)));
+    })
+
 
     /*** delete a publication from the database ***/
     this.delete(PublicationEndpoints.remove, (req, res, next) => {
@@ -102,9 +184,10 @@ class PublicationRouterFactory extends RouterFactoryImp {
         id: req.body.id,
         user_id: req.body.user_id
       }
+      const cred = req.body.credentials;
       const credentials = {
-        username: req.body.username,
-        password: req.body.password
+        username: cred ? cred.username : undefined,
+        password: cred ? cred.password : undefined
       }
       if (this.hasUndefined(reqBody, credentials)) {
         next(BadRequest);
@@ -115,6 +198,10 @@ class PublicationRouterFactory extends RouterFactoryImp {
 
       this.queryManager.query(async () => {
         const user = await this.queryManager.users.get(reqBody.user_id);
+        if (!user.id) {
+          next(NotFound);
+          return;
+        }
         if (!this.auth(user, credentials)) {
           next(AuthenticationFailed);
           return;
@@ -128,7 +215,7 @@ class PublicationRouterFactory extends RouterFactoryImp {
         res.json(Done());
       })
       .execute()
-      .catch(e => {console.log(e); next(DBError(e.code))});
+      .catch(e => next(DBError(e.code)));
     })
   }
 }
