f3e9fbca41a45137fa3f9bf26e8e3256adbea56f
Mahmoud Ehab <mo.ehab150@gmail.com>
2022-09-11 02:07:46 +0200

    getPublicationWithLimit, getPublicationWithLimitAndOffset, and update Endpoints have been added

--- backend/src/server/Endpoints/Collections/PublicationEndpoints.ts
+++ backend/src/server/Endpoints/Collections/PublicationEndpoints.ts
@@ -6,12 +6,29 @@ export const getPublication: Endpoint = {
   type: 'GET',
 }
 
+export const getPublicationsWithLimit: Endpoint = {
+  path: (limit: number) => `/publication/limit/${limit}`,
+  expressPath: '/limit/:limit',
+  type: 'GET',
+}
+
+export const getPublicationsWithLimitAndOffset: Endpoint = {
+  path: (limit: number, offset: number) => `/publication/limit/${limit}/offset/${offset}`,
+  expressPath: '/limit/:limit/offset/:offset',
+  type: 'GET',
+}
+
 export const getPublicationsOfUser: Endpoint = {
   path: (userid) => `/publication/of/user/${userid}`,
   expressPath: '/of/user/:userid',
   type: 'GET',
 }
 
+export const update: Endpoint = {
+  path: () => '/publication/update',
+  expressPath: '/update',
+  type: 'PATCH',
+}
 export const create: Endpoint = {
   path: () => '/publication/create',
   expressPath: '/create',
