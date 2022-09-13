4e2d15d51793323d4baa9ee725e53e5522216255
Mahmoud Ehab <mo.ehab150@gmail.com>
2022-09-14 00:38:40 +0200

    PublicationStrategy.ts appled (just committed late)

---
+++ backend/src/server/QueriesStrategy/strategies/PublicationStrategy.ts
@@ -0,0 +1,4 @@
+import { Publication } from "../../../database/DataController/controllers/PublicationsController";
+import { QueriesStrategyImp } from "../QueriesStrategyImp";
+
+export class PublicationStrategy extends QueriesStrategyImp<Publication> {}
