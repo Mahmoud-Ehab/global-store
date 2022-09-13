cc89d48d24ce57641242ed7897dfe63d900db902
Mahmoud Ehab <mo.ehab150@gmail.com>
2022-09-13 14:28:36 +0200

    Controllers type and getController method are declared and exported to be used by QueriesStrategy in the Server Component

--- backend/src/database/QueryManager/QueryManager.ts
+++ backend/src/database/QueryManager/QueryManager.ts
@@ -2,10 +2,20 @@ import PublicationsController from "../DataController/controllers/PublicationsCo
 import ReviewsController from "../DataController/controllers/ReviewsController";
 import UsersController from "../DataController/controllers/UsersController";
 
+export type Controllers = {
+  users: UsersController,
+  publications: PublicationsController,
+  reviews: ReviewsController
+}
+
 interface QueryManager {
   get users(): UsersController;
   get publications(): PublicationsController;
   get reviews(): ReviewsController;
+  getController(type: keyof Controllers): 
+    UsersController | 
+    PublicationsController | 
+    ReviewsController;
   query(func: () => Promise<any>): QueryManager;
   execute(): Promise<void>;
 }
