07f9eadfe96e6ee32b87720c03c60537fa93b6ef
Mahmoud Ehab <mo.ehab150@gmail.com>
2022-09-14 00:53:14 +0200

    ReviewStrategy has been added to strategy to be used by ReviewRouterInitializer

---
+++ backend/src/server/QueriesStrategy/strategies/ReviewStrategy.ts
@@ -0,0 +1,4 @@
+import { Review } from "../../../database/DataController/controllers/ReviewsController";
+import { QueriesStrategyImp } from "../QueriesStrategyImp";
+
+export class ReviewStrategy extends QueriesStrategyImp<Review> {}
