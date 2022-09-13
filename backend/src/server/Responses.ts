f4c54ab67b0ffdacd4255789ccff7f61fd96223c
Mahmoud Ehab <mo.ehab150@gmail.com>
2022-09-13 14:44:38 +0200

    Responses moved to server directory and InternalError has been added.

---
+++ backend/src/server/Responses.ts
@@ -0,0 +1,48 @@
+
+type Response = {
+  code: number,
+  message: string,
+  metadata?: object,
+}
+
+export const Done = (metadata?: object) => ({
+  code: 200,
+  message: "query fulfilled.", 
+  metadata,
+})
+
+export const Authenticated: Response = {
+  code: 200,
+  message: "authenticated.", 
+  metadata: {
+    auth: true
+  }
+}
+
+export const BadRequest: Response = {
+  code: 400,
+  message: "The request cannot be fulfilled, the request data is insufficient or wrong.", 
+}
+
+export const AuthenticationFailed: Response = {
+  code: 401,
+  message: "authentication failed.", 
+  metadata: {
+    auth: false
+  }
+}
+
+export const NotFound: Response = {
+  code: 404,
+  message: "requested resource not found.",
+}
+
+export const AlreadyExists: Response = {
+  code: 409,
+  message: "Entry already exists.", 
+}
+
+export const InteranlError: Response = {
+  code: 500,
+  message: "Internal error.", 
+}
