3e2b9e4443b117dd3081a23a99aaaa402dbddfc4
Mahmoud Ehab <mo.ehab150@gmail.com>
2022-09-13 14:48:09 +0200

    UserFactory has been modified to adhere the new architecture with QueriesStrategy implementation

--- backend/src/server/RouterFactory/factories/UserRouterFactory.ts
+++ backend/src/server/RouterFactory/factories/UserRouterFactory.ts
@@ -1,38 +1,32 @@
 import { UserEndpoints } from "../../Endpoints";
+import { UserStrategies } from "../../QueriesStrategy/strategies/UserStrategies";
 import { 
   Authenticated,
-  AuthenticationFailed, 
   BadRequest, 
-  DBError,
   Done,
-  NotFound, 
-} from "../Responses";
+} from "../../Responses";
 import RouterFactoryImp from "../RouterFactoryImp";
 
 class UserRouterFactory extends RouterFactoryImp {
   _routerName = "user";
 
   init() {
+    const qs = new UserStrategies(this.queryManager, "users");
+
     /*** Get User with a specific id ***/
     this.get(UserEndpoints.getUser, (req, res, next) => {
       const userid = req.params.userid;
 
-      this.queryManager.query(async () => {
-        const result = await this.queryManager.users.get(userid);
-        if (!result.id) {
-          next(NotFound);
-          return;
-        }
-          
-        result.password = undefined;
-        res.json(Done({data: result}));
-      })
+      this.queryManager
+      .query(qs.getById(userid))
+      .query(qs.ifExists())
+      .query(qs.send(res))
       .execute()
-      .catch(e =>next(DBError(e.code)));
+      .catch(e => next(e));
     });
 
 
-
+    
     /*** Get list of a limited number of users ***/
     this.get(UserEndpoints.getUsersLimit, (req, res, next) => {
       const limit = parseInt(req.params.limit);
@@ -40,17 +34,11 @@ class UserRouterFactory extends RouterFactoryImp {
         next(BadRequest)
         return; 
       }
-
-      this.queryManager.query(async () => {
-        const result = await this.queryManager.users.getLimit(limit);
-
-        for (const user of result)
-          user.password = undefined;
-
-        res.json(Done({data: result}));
-      })
+      this.queryManager
+      .query(qs.getLimit(limit))
+      .query(qs.send(res))
       .execute()
-      .catch(e =>next(DBError(e.code)));
+      .catch(e => next(e));
     });
 
 
@@ -68,23 +56,13 @@ class UserRouterFactory extends RouterFactoryImp {
 
       //@TODO: Encrypt the password in reqBody obj
 
-      this.queryManager.query(async () => {
-        const userQuery = await this.queryManager.users.getFiltered({
-          username: reqBody.username
-        });
-        if (!userQuery[0]) {
-          next(NotFound);
-          return;
-        }
-        const auth = await this.queryManager.users.auth(userQuery[0].id, reqBody);
-        if (!auth) {
-          next(AuthenticationFailed);
-          return;
-        }
-        res.json(Authenticated);
-      })
+      this.queryManager
+      .query(qs.getFilteredList({username: reqBody.username}))
+      .query(qs.ifExists())
+      .query(qs.authUser(reqBody))
+      .query(async () => res.json(Authenticated))
       .execute()
-      .catch(e => next(DBError(e.code)));
+      .catch(e => next(e));
     });
 
 
@@ -103,12 +81,13 @@ class UserRouterFactory extends RouterFactoryImp {
 
       //@TODO: Encrypt the password in reqBody obj
 
-      this.queryManager.query(async () => {
-          await this.queryManager.users.insert(reqBody);
-          res.json(Done({id: reqBody.id}));
-      })
+      this.queryManager
+      .query(qs.getFilteredList({username: reqBody.username}))
+      .query(qs.ifNotExists())
+      .query(qs.insert(reqBody))
+      .query(async () => res.json(Done({id: reqBody.id})))
       .execute()
-      .catch(e => next(DBError(e.code)));
+      .catch(e => next(e));
     });
 
 
@@ -134,18 +113,14 @@ class UserRouterFactory extends RouterFactoryImp {
       }
 
       //@TODO: Encrypt the password in reqBody obj
-
-      this.queryManager.query(async () => {
-        const auth = await this.queryManager.users.auth(reqBody.id, credentials);
-        if (!auth) {
-          next(AuthenticationFailed);
-          return;
-        }
-        await this.queryManager.users.update(reqBody.data, {id: reqBody.id});
-        res.json(Done());
-      })
+      this.queryManager
+      .query(qs.getById(reqBody.id))
+      .query(qs.ifExists())
+      .query(qs.authUser(credentials))
+      .query(qs.update(reqBody.data, {id: reqBody.id}))
+      .query(async () => res.json(Done()))
       .execute()
-      .catch(e => next(DBError(e.code)));
+      .catch(e => next(e));
     });
 
 
@@ -166,27 +141,18 @@ class UserRouterFactory extends RouterFactoryImp {
       }
 
       //@TODO: Encrypt the password in reqBody obj
-
-      this.queryManager.query(async () => {
-        const user = await this.queryManager.users.get(reqBody.id);
-        if (!user.id) {
-          next(NotFound);
-          return;
-        }
-        const auth = await this.queryManager.users.auth(reqBody.id, credentials);
-        if (!auth) {
-          next(AuthenticationFailed);
-          return;
-        }
-        await this.queryManager.users.delete({id: reqBody.id});
-        res.json(Done());
-      })
+      this.queryManager
+      .query(qs.getById(reqBody.id))
+      .query(qs.ifExists())
+      .query(qs.authUser(credentials))
+      .query(qs.delete({id: reqBody.id}))
+      .query(async () => res.json(Done()))
       .execute()
-      .catch(e => next(DBError(e.code)));
+      .catch(e => next(e));
     });
   }
 
-  private generateId = (username: string): string => {
+  private generateId(username: string): string {
     const randnum = Math.floor(Math.random() * 10000);
     return username.slice(0, 2) + randnum;
   }
