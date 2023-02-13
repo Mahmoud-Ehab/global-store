6b1d3b89afd6795ade2301abdffc3a4de30ad654
Mahmoud Ehab <mo.ehab150@gmail.com>
2023-02-13 23:22:11 +0200

    The root state class has been implemented

---
+++ frontend/src/states/index.ts
@@ -0,0 +1,24 @@
+import { Entity, Controller, AppState } from "../modules/StateManager"
+
+type AnyEntity = Entity<string, any, any>;
+
+type RootEntity = Entity<string, AppState<AnyEntity>, AppState<AnyEntity>>;
+class RootController extends Controller<AnyEntity> {}
+
+class RootState extends AppState<RootEntity> {
+  constructor() {
+		super();
+		this.controller = new RootController(this.entities);
+	}
+
+  protected needsUpdate(key: AnyEntity["key"], newValue: AnyEntity["value"]): boolean {
+    const value = this.controller.getValue(key);
+    return !value.isEqual(newValue);
+  }
+
+  protected generateCache(value: RootEntity["value"]): RootEntity["cache"] {
+    return value;
+  }
+}
+
+export const StateManager = new RootState();
