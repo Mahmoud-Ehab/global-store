160ea7f03f09798b2aa47aa567ab694834d1e138
Mahmoud Ehab <mo.ehab150@gmail.com>
2023-02-26 19:06:17 +0200

    Headers type is used in the Dispatcher.

--- frontend/src/modules/RequestDispatcher/Dispatcher.ts
+++ frontend/src/modules/RequestDispatcher/Dispatcher.ts
@@ -1,11 +1,10 @@
 import { Request } from "./types/Request";
 import { Response } from "./types/Response";
 import { Proxy } from "./types/Proxy";
+import { Headers } from "./types/Headers";
 
 export interface Dispatcher {
   dispatch(req: Request): Response;
-  getHeaders(): string;
+  getHeaders(): Headers;
   getProxy(): Proxy;
-  setHeaders(headers: string): void;
-  setProxy(proxy: Proxy): void;
 }
